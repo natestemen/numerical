f102e2c58f3984f7a16be3dff6d4667f558b6d71
nate stemen <nate@stemen.email>
2020-11-08 22:11:34 -0500

    simplify construction of diagonally dominant matrix
    also don't use ord=2 for norms bc it's the default

--- assignment2/GaussSeidel.py
+++ assignment2/GaussSeidel.py
@@ -10,7 +10,7 @@ def GaussSeidel(A, guess, b, tolerance, maxIterations):
             new_sum = np.dot(A[i, :i], x_new[:i])
             old_sum = np.dot(A[i, i + 1 :], x_old[i + 1 :])
             x_new[i] = (b[i] - new_sum - old_sum) / A[i, i]
-        if np.linalg.norm(x_new - x_old, ord=2) < tolerance:
+        if np.linalg.norm(x_new - x_old) < tolerance:
             break
         x_old = x_new
     if iteration == maxIterations:
--- assignment2/VerifyGaussSeidel.py
+++ assignment2/VerifyGaussSeidel.py
@@ -7,13 +7,10 @@ A = np.random.rand(n, n)
 b = np.random.rand(n)
 
 for i in range(n):
-    c = 0
-    for j in range(n):
-        c += A[i, j]
-    A[i, i] = c
+    A[i, i] = sum(A[i, j] for j in range(n))
 
 x = GaussSeidel(A, np.zeros(n), b, 1e-12, 1000)
 
-error = np.linalg.norm(x - np.linalg.solve(A, b), ord=2)
+error = np.linalg.norm(x - np.linalg.solve(A, b))
 
 print(f"total error: {error}")
