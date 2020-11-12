d3e1cc202861ffeb52218b435098a6cd8a61e6f2
nate stemen <nate@stemen.email>
2020-11-12 09:17:35 -0500

    ayy got preconditioning to work!

--- assignment2/myGMRESSSOR.py
+++ assignment2/myGMRESSSOR.py
@@ -2,10 +2,43 @@ import numpy as np
 
 
 def myGMRES_SSOR(A, guess, b, tolerance=1e-12, maxIterations=1000):
-    """Performs a sequence of right-preconditioned GMRES iterations on A x = b
-    with the initial estimate x0, using the SSOR preconditioner, until the
-    2-norm of the residual is smaller than tol(relative to the initial
-    residual), or until the number of iterations reaches maxit. ‘steps’ is the
-    number of steps that were performed, and ‘res’ is a vector with the
-    residual size after every interation(the 2-norm of the residual vector)."""
+    n = A.shape[0]
+    omega = 1.9
+    AL = sparse.tril(A, k=-1)
+    AU = sparse.triu(A, k=1)
+    AD = sparse.spdiags(A.diagonal(), [0], n, n)
+    ADinv = sparse.spdiags([1 / x for x in A.diagonal()], [0], n, n)
+    Pinv = (AD - omega * AL) @ ADinv @ (AD - omega * AU) / (omega * (2 - omega))
+
+    r0 = b - A @ guess
+    rho = np.linalg.norm(r0)
+    r0 /= rho
+
+    Q = np.zeros((n, maxIterations))
+    Q[:, 0] = r0
+    H = sparse.lil_matrix((maxIterations + 1, maxIterations))
+    residuals = []
+    for iteration in range(maxIterations):
+        z = sparse.linalg.spsolve(Pinv, Q[:, iteration])
+        v = A @ z
+        for j in range(iteration + 1):
+            H[j, iteration] = np.dot(Q[:, j], v)
+            v -= H[j, iteration] * Q[:, j]
+
+        norm_v = np.linalg.norm(v)
+        H[iteration + 1, iteration] = norm_v
+        Q[:, iteration + 1] = v / norm_v
+
+        e = np.zeros(maxIterations + 1)
+        e[0] = rho
+        y, _, _, residual, *_ = sparse.linalg.lsqr(H, e)
+        residual /= rho
+        residuals.append(residual)
+
+        if residual < tolerance:
+            w = sparse.linalg.spsolve(Pinv, Q @ y)
+            x = guess + w
+            break
+
+    return x, residuals, iteration + 1
 
